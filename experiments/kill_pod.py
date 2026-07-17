aec8fc09ff38e1ff7e93e0fb3f225c9779af4833
m1rua <vagnomail@mail.ru>
2026-07-17 20:33:00 +0300

    added kill_pod.py experiment

---
+++ experiments/kill_pod.py
@@ -0,0 +1,13 @@
+from kubernetes import client, config
+import random
+
+config.load_kube_config()
+
+v1 = client.CoreV1Api()
+namespace = "default"
+label_selector = "app=main-app"
+
+pods = v1.list_namespaced_pod(namespace=namespace, label_selector=label_selector)
+random_pod = random.choice(pods.items)
+deleting_pod_name = random_pod.metadata.name
+v1.delete_namespaced_pod(deleting_pod_name)
