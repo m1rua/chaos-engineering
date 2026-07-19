809fe5ac5c1734fa95b910cc21c19eb69212b274
m1rua <vagnomail@mail.ru>
2026-07-19 18:37:26 +0300

    added cpu_stress.py experiment

---
+++ experiments/cpu_stress.py
@@ -0,0 +1,18 @@
+from kubernetes import client, config
+from kubernetes.stream import stream
+import random
+config.load_kube_config()
+v1 = client.CoreV1Api()
+
+namespace = "default"
+label_selector = "app=main-app"
+
+pods = v1.list_namespaced_pod(namespace=namespace, label_selector=label_selector)
+random_pod = random.choice(pods.items)
+cpu_stress_pod = random_pod.metadata.name
+
+cpu_stress_script = ["python3", "-c", "while True: 1+1"]
+
+result = stream(v1.connect_get_namespaced_pod_exec,name=cpu_stress_pod, namespace=namespace, command=cpu_stress_script, stderr=True, stdin=False, stdout=True, tty=False)
+
+print(result)
