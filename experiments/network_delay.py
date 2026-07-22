41f2cf71e6ad407e04b76e51a1ce3c34f0abbe59
m1rua <vagnomail@mail.ru>
2026-07-22 11:56:54 +0300

    fixed network_delay experiment

--- experiments/network_delay.py
+++ experiments/network_delay.py
@@ -12,11 +12,10 @@ pods = v1.list_namespaced_pod(namespace=namespace, label_selector=label_selector
 random_pod = random.choice(pods.items)
 network_delay_pod = random_pod.metadata.name
 network_delay_script = ["sh", "-c", "tc qdisc add dev eth0 root netem delay 120ms"]
-network_delay_fix_script = ["sh", "-c", "tc qdisc delete dev eth0 root netem delay 120ms"]
+network_delay_fix_script = ["sh", "-c", "tc qdisc delete dev eth0 root"]
 
-duration = 30
-start_time = time.time
+print("был выбран ", network_delay_pod)
 
 stream (v1.connect_get_namespaced_pod_exec, name=network_delay_pod, namespace=namespace, command=network_delay_script, stderr=True, stdin=False, stdout=True, tty=False)
 time.sleep(30)
-stream (v1.connect_get_namespaced_pod_exec, name=networl_delay_pod, namespace=namespace, command=netowork_delay_fix_script, stderr=True, stdin=False, stdout=True, tty=False)
+stream (v1.connect_get_namespaced_pod_exec, name=network_delay_pod, namespace=namespace, command=network_delay_fix_script, stderr=True, stdin=False, stdout=True, tty=False)
