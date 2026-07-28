289e6ea5ebd6762e90e1beb3c8b91ee15958a81e
m1rua <vagnomail@mail.ru>
2026-07-28 16:52:14 +0300

    updated pod_autoscale.py

--- experiments/pod_autoscale.py
+++ experiments/pod_autoscale.py
@@ -1,16 +1,14 @@
-import requests
-import time
 from kubernetes import config, client
 from kubernetes.stream import stream
-from concurrent.futures import ThreadPoolExecutor
 
 config.load_kube_config()
 v1 = client.CoreV1Api()
 
-start_time = time.time()
-duration = 30
+namespace = 'default'
+label_selector = "app=main-app"
+pods = v1.list_namespaced_pod(namespace=namespace, label_selector=label_selector)
+autoscale_pod = pods.items
+autoscale_pod_name = autoscale_pod[0].metadata.name
+http_script = "python3", "-c", "import time; import requests; from concurrent.futures import ThreadPoolExecutor; start_time = time.time(); duration = 30; \nwhile time.time() - start_time < duration: \nwith TreadPoolExecutor(max_workers=100) as executor: \nfor i in range(100): executor.submit(request.get, 'http://main-app:80/health')"
 
-while time.time() - start_time < duration:
-    with ThreadPoolExecutor(max_workers=100) as executor:
-        for i in range(100):
-            executor.submit(requests.get, "http://localhost:8000/health")
+stream(v1.connect_get_namespaced_pod_exec, namespace=namespace, name=autoscale_pod_name, command=http_script, stderr=True, stdin=False, stdout=True, tty=False)
