1e55ba8a4ecea375424c4025dc57c9ac42d4ab21
m1rua <vagnomail@mail.ru>
2026-07-29 20:13:05 +0300

    fixed pod_autoscale.py

--- experiments/pod_autoscale.py
+++ experiments/pod_autoscale.py
@@ -9,6 +9,6 @@ label_selector = "app=main-app"
 pods = v1.list_namespaced_pod(namespace=namespace, label_selector=label_selector)
 autoscale_pod = pods.items
 autoscale_pod_name = autoscale_pod[0].metadata.name
-http_script = "python3", "-c", "import time; import requests; from concurrent.futures import ThreadPoolExecutor; start_time = time.time(); duration = 30; \nwhile time.time() - start_time < duration: \nwith TreadPoolExecutor(max_workers=100) as executor: \nfor i in range(100): executor.submit(request.get, 'http://main-app:80/health')"
+http_script = "python3", "-c", "import time; import requests; from concurrent.futures import ThreadPoolExecutor; start_time = time.time(); duration = 30; \nwhile time.time() - start_time < duration: \n    with TreadPoolExecutor(max_workers=100) as executor: \n        for i in range(100): executor.submit(request.get, 'http://main-app:80/health')"
 
 stream(v1.connect_get_namespaced_pod_exec, namespace=namespace, name=autoscale_pod_name, command=http_script, stderr=True, stdin=False, stdout=True, tty=False)
