09a60dbd6a6646cfffb30f667cc3f6d11610dfb8
m1rua <vagnomail@mail.ru>
2026-07-31 23:54:29 +0300

    update pod_autoscale.py

--- experiments/pod_autoscale.py
+++ experiments/pod_autoscale.py
@@ -11,4 +11,5 @@ autoscale_pod = pods.items
 autoscale_pod_name = autoscale_pod[0].metadata.name
 http_script = ["python3", "-c", "import time; import requests; from concurrent.futures import ThreadPoolExecutor; start_time = time.time(); duration = 30; \nwhile time.time() - start_time < duration: \n    with ThreadPoolExecutor(max_workers=100) as executor: \n        for i in range(100): executor.submit(requests.get, 'http://main-app:80/health')"]
 
-stream(v1.connect_get_namespaced_pod_exec, namespace=namespace, name=autoscale_pod_name, command=http_script, stderr=True, stdin=False, stdout=True, tty=False)
+result = stream(v1.connect_get_namespaced_pod_exec, namespace=namespace, name=autoscale_pod_name, command=http_script, stderr=True, stdin=False, stdout=True, tty=False)
+print(result)
